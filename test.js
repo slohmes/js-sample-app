8799fc8f1d30539f7b614e689e0e799a345cfe67
slohmes <sarahlohmes@gmail.com>
2018-01-08 11:49:40 -0800

    tests localhost instead of example page

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    //headless: false,
+    userDataDir: '/Users/sarahlohmeier/projects/js-sample-app/tmp/mockUserDataDir'
+  });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'tmp/example.png'}); // if there's already an image saved, what happens?
+  await page.goto('http://localhost:3000');
+  await page.waitForFunction('document.querySelector("#demo").innerHTML.length > 0').catch(err => console.log("something's wrong", err));
+  await page.screenshot({path: 'tmp/localhost3000-4.png'}); // if there's already an image saved, what happens?
 
   console.log("saved screenshot");
   await browser.close();
